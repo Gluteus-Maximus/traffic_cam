1ac0fc8e4ac90c27d0e9af0fbae412be82c20420
Derek Shea <derek.t.shea.apps@gmail.com>
2019-10-29 20:30:10 -0400

    Add initial parser.

---
+++ Traffic_Cam.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from pathlib import Path
+
+def parse_netdev(interface):
+  '''
+  @return: Dictionary of network traffic values.  Values are cumulative for the
+    life of the operating system.  All values are int's.
+    keys: rx_bytes, rx_pkts, tx_bytes, tx_pkts
+  @param interface: The network interface for which network traffic info will
+    be gathered.
+  '''
+  netdev = Path('/proc/net/dev')
+  trafficRaw = netdev.read_text()
+  idxZero = trafficRaw.index(interface + ":")
+  traffic = dict([
+      ('rx_bytes', trafficRaw[idxZero + 1]),
+      ('rx_pkts', trafficRaw[idxZero + 2]),
+      ('tx_bytes', trafficRaw[idxZero + 9]),
+      ('tx_pkts', trafficRaw[idxZero + 10])
+      ])
+  return traffic
