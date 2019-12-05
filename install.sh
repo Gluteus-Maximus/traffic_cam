f4871dd3343d16f95cf910a2230b42c73e4df490
Gluteus-Maximus <derek.t.shea.apps@gmail.com>
2019-12-04 22:48:34 -0500

    Pre-config program file.

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # Run from same directory as program file
 PROG_FILE=$(pwd)/traffic-cam
 CONF_FILE=$(pwd)/.traffic-cam.conf
-ERR_FILE=/var/log/test_traffic-cam.log
+ERR_FILE=/var/log/traffic-cam.log
 PATH_FILE=/etc/profile.d/traffic-cam.sh
 
-#TODO: echo messages
+#TODO: check results before 'done'
 echo "Create config file:    '$CONF_FILE'"
 touch $CONF_FILE
 chmod 664 $CONF_FILE
@@ -18,10 +18,11 @@ sudo chmod 664 $ERR_FILE
 echo "   done"
 # Add global PATH update to /etc/profile.d/
 echo "Add program to PATH:   '$PATH_FILE'"
+sudo touch $PATH_FILE
 # Source profile.d?? How??
 echo "   done"
 echo "Pre-configure program: '$PROG_FILE'"
-# sudo chown root traffic-cam
-# sudo chgrp root traffic-cam
-# sudo chmod +x traffic-cam ##Specify permissions 755
+sudo chown root $PROG_FILE
+sudo chgrp root $PROG_FILE
+sudo chmod 755 $PROG_FILE # all users: read/exe, only root: write
 echo "   done"
