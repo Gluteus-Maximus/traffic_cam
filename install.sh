895d7ca3d0d47940b51de99b69ae7b83cd10e880
Gluteus-Maximus <derek.t.shea.apps@gmail.com>
2019-12-04 21:17:54 -0500

    Change stubs for install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,12 @@
+#!/usr/bin/bash
 # Recommend run as normal user to allow unprivileged use
 
-# Add PATH update to ~/.bashrc
-### Global bashrc? (/etc/profile.d/)
-### Needs to update root bashrc, so run as root (alert and quit if not root)
-# Delete/Update if exists, else append
-
-# Limit comment begin
-# PATH Update Logic
-# Limit comment end
-
-# Source bashrc?? How??
 # Touch config
 # Touch error log?
-### Store Sudo Creds ###
+### Sudo Commands ###
+# Source profile.d?? How??
+# Add global PATH update to /etc/profile.d/
+# Get root credentials
 # sudo chown root traffic-cam
 # sudo chgrp root traffic-cam
 # sudo chmod +x traffic-cam ##Specify permissions 755
