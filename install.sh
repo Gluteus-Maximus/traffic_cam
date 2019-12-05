e694077a55300214b221da762b8d170af0a70ca9
Gluteus-Maximus <derek.t.shea.apps@gmail.com>
2019-12-04 22:03:50 -0500

    Filepath variables, touch/chmod.

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/bash
 # Recommend run as normal user to allow unprivileged use
+PROG_FILE=traffic-cam
+CONF_FILE=.traffic-cam.conf
+ERR_FILE=/var/log/traffic-cam.log
 
-# Touch config
-# Touch error log?
+#TODO: echo messages
+touch $CONF_FILE
+chmod 666 $CONF_FILE
+touch $ERR_FILE
+chmod 666 $ERR_FILE
 ### Sudo Commands ###
-# Source profile.d?? How??
 # Add global PATH update to /etc/profile.d/
+# Source profile.d?? How??
 # Get root credentials
 # sudo chown root traffic-cam
 # sudo chgrp root traffic-cam
