8809acb6d9d067ba74e3cc15bece2f469015c45a
Gluteus-Maximus <derek.t.shea.apps@gmail.com>
2019-12-05 23:43:46 -0500

    Remove profile.d method, replace with sym link for global cli calls.

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 PROG_FILE=$(pwd)/traffic-cam
 CONF_FILE=$(pwd)/.traffic-cam.conf
 ERR_FILE=/var/log/traffic-cam.log
-PATH_FILE=/etc/profile.d/traffic-cam.sh
+SYM_LINK=/usr/local/bin/traffic-cam  # cli command defined by name of sym link
+#PATH_FILE=/etc/profile.d/traffic-cam.sh
 
 #TODO: check results before 'done'
 # Add Config File
@@ -19,6 +20,7 @@ sudo touch $ERR_FILE
 sudo chmod 664 $ERR_FILE
 echo "   done"
 
+'''
 # Add global PATH update to /etc/profile.d/
 echo "Add program to PATH: '$PATH_FILE'"
 #sudo touch $PATH_FILE
@@ -30,6 +32,9 @@ CONTENT+="\nPATH=\$PATH:$(pwd)"
 sudo sh -c "echo '$CONTENT' > $PATH_FILE"
 source $PATH_FILE  #TODO: source not working
 echo "   done"
+'''
+# Add Symbolic Link
+sudo ln -s $PROG_FILE $SYM_LINK
 
 # Pre-Configure Program File (permissions)
 echo "Configure program:   '$PROG_FILE'"
