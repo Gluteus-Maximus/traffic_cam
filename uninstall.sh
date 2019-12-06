2e991d1eefbf8affebfc9aad0964f81d3aa75985
Gluteus-Maximus <derek.t.shea.apps@gmail.com>
2019-12-06 00:29:52 -0500

    Add symlink removal.

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ERR_FILE=/var/log/traffic-cam.log
-PATH_FILE=/etc/profile.d/traffic-cam.sh
+SYM_LINK=/usr/local/bin/traffic-cam
+#PATH_FILE=/etc/profile.d/traffic-cam.sh
 
 sudo rm $ERR_FILE
-sudo rm $PATH_FILE
+#sudo rm $PATH_FILE
+sudo rm $SYM_LINK
