d57b8309ba9deacb91c6fdca52ecc79502129cf1
Gluteus-Maximus <derek.t.shea.apps@gmail.com>
2019-12-16 18:08:01 -0500

    Remove cronjob during uninstall.

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,8 @@
 # Remove traffic-cam artifacts from filesystem
 ERR_FILE=/var/log/traffic-cam.log
 SYM_LINK=/usr/local/bin/traffic-cam
+CRON_FILE=/etc/cron.d/traffic-cam-cron
 
 sudo rm $ERR_FILE
 sudo rm $SYM_LINK
+sudo rm $CRON_FILE
